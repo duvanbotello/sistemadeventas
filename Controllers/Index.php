7c5fa04f1f186e715ff9b518966f7c3ef14196f8
Duvan Botello <this.duvan@gmail.com>
2019-06-29 16:28:38 -0500

    Ejecutando la vista index, y agregando el framwork

---
+++ Controllers/Index.php
@@ -0,0 +1,15 @@
+<?php
+    class Index extends Controllers
+    {
+        public function __construct() {
+            //ejecutamos el metodo constructor de la clase padre.
+            parent::__construct();
+        }
+        public function index(){
+            //ejecuto el metodo render de la libreria Views le paso el cotrolador y la vista como parametro
+            $this->view->render($this,"index");
+        }
+
+    }
+    
+?>
